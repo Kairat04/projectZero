f6bd96397edae78c781982ab2a0ea012aa934dd0
kairat_tunkatarov <kairat05062004@gmail.com>
2022-11-15 01:16:11 +0600

    "first commit"

---
+++ src/Anime.java
@@ -0,0 +1,11 @@
+public class Anime {
+    String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Wassup");
+        Anime anime = new Anime();
+        anime.setName("Aizen");
+        System.out.println(anime.getName());
+
+    }
+}
